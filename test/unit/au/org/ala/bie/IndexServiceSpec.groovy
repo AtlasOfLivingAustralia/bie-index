aad814c33920614d14667d1c82a64dda444d1018
Simon Bear <simon.bear@csiro.au>
2016-01-14 17:15:16 +1100

    rename IndexServiceTest to IndexServiceSpec for consistency

--- test/unit/au/org/ala/bie/IndexServiceSpec.groovy
+++ test/unit/au/org/ala/bie/IndexServiceSpec.groovy
@@ -12,7 +12,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(IndexService)
-class IndexServiceTest extends Specification {
+class IndexServiceSpec extends Specification {
 
     SolrClient liveSolrClient
 
