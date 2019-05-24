d51b791f4fdc2443a517e1368c246ba26b24011e
Nick dos Remedios <nick.dosremedios@csiro.au>
2019-05-25 09:18:10 +1000

    Updated KnowledgeBaseServiceSpec tests

--- src/test/groovy/au/org/ala/bie/KnowledgeBaseServiceSpec.groovy
+++ src/test/groovy/au/org/ala/bie/KnowledgeBaseServiceSpec.groovy
@@ -12,39 +12,50 @@ class KnowledgeBaseServiceSpec extends Specification {
     static List pages = []
     static String firstPageUrl = ""
     static String randomPageUrl = ""
+    static Map firstPageMap = [:]
     static Map randomPageMap = [:]
     static int randomNum
 
     def setupSpec() {
+        // call web service once only and store results in static vars
         pages = service.resources("")
         firstPageUrl = pages.get(0)
+        firstPageMap = service.getResource(firstPageUrl)
         Random rand = new Random()
         randomNum = rand.nextInt(101)
         randomPageUrl = pages.get(randomNum)
         randomPageMap = service.getResource(randomPageUrl)
 
-        log.warn "resources is ${pages.size()} in size"
-        log.warn "first page URL = ${firstPageUrl}"
-        log.warn "random page URL = ${randomPageUrl}"
-        log.warn "random page Map = ${randomPageMap}"
+        log.info "resources is ${pages.size()} in size"
+        log.info "first page URL = ${firstPageUrl}"
+        log.info "random page URL = ${randomPageUrl}"
+        log.info "random page Map = ${randomPageMap}"
     }
 
     def cleanup() {
     }
 
-    void "test resources should not be empty"() {
+    void "test resources (List) should not be empty"() {
         expect:
             pages.size() > 1
     }
 
-    void "test first page has expected domain"() {
+    void "test first page URL has expected domain"() {
         expect:
             firstPageUrl?.startsWith("https://support.ala.org.au")
     }
 
-    void "test random page has expected domain"() {
+    void "test random page URL has expected domain"() {
         expect:
             randomPageUrl?.startsWith("https://support.ala.org.au")
+    }
+
+    void "test first page has body text"() {
+        expect:
+            firstPageMap.containsKey("id")
+            firstPageMap.containsKey("title")
+            firstPageMap.containsKey("body")
+            firstPageMap.get("body").size() > 100
     }
 
     void "test random page has body text"() {
