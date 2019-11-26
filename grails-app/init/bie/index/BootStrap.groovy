678a73e7c95af59a7c699ec471474db9f9e5b03c
vjrj <vjrj@ourproject.org>
2019-11-26 12:00:07 +0100

    Usage of external ala-i18n resources

--- grails-app/init/bie/index/BootStrap.groovy
+++ grails-app/init/bie/index/BootStrap.groovy
@@ -1,8 +1,15 @@
 package bie.index
 
 class BootStrap {
+    def messageSource
 
     def init = { servletContext ->
+        messageSource.setBasenames(
+                "file:///var/opt/atlas/i18n/bie-index/messages",
+                "file:///opt/atlas/i18n/bie-index/messages",
+                "WEB-INF/grails-app/i18n/messages",
+                "classpath:messages"
+        )
     }
     def destroy = {
     }
