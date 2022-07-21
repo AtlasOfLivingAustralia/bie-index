016e6a7b6a121922c54e4c77369d0ccce2d1801c
Sushant <sushant.ghimire@csiro.au>
2022-07-22 09:17:54 +1000

    issue #349 uncommenting AlaSecured annotations, removing unused imports

--- grails-app/controllers/au/org/ala/bie/AdminController.groovy
+++ grails-app/controllers/au/org/ala/bie/AdminController.groovy
@@ -1,9 +1,7 @@
 package au.org.ala.bie
 import au.org.ala.web.AlaSecured
-import grails.converters.JSON
-import grails.converters.XML
 
-//@AlaSecured(value = "ROLE_ADMIN", redirectUri = "/")
+@AlaSecured(value = "ROLE_ADMIN", redirectUri = "/")
 class AdminController {
 
     def index() {} // GSP only index
