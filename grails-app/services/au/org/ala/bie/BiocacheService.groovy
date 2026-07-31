6a3d019a5ce0160f2fe589122d5ddb69d7346fd0
Nick dos Remedios <nick.dosremedios@csiro.au>
2026-07-31 11:19:14 +1000

    #403 Minor improvement to DQ filter code
    Making code more LA friendly @vjrj

--- grails-app/services/au/org/ala/bie/BiocacheService.groovy
+++ grails-app/services/au/org/ala/bie/BiocacheService.groovy
@@ -85,8 +85,10 @@ class BiocacheService {
             query << "sort=${Encoder.escapeQuery(sort)}"
             query << "dir=${Encoder.escapeQuery(dir)}"
         }
-        def qualityProfile = grailsApplication.config.getProperty("dataQuality.defaultProfile", String, "ALA")
-        query << "qualityProfile=${Encoder.escapeQuery(qualityProfile)}"
+        if (grailsApplication.config.getProperty("dataQuality.includeFilter", Boolean, true)) {
+            def qualityProfile = grailsApplication.config.getProperty("dataQuality.profile", String, "ALA")
+            query << "qualityProfile=${Encoder.escapeQuery(qualityProfile)}"
+        }
         def url = grailsApplication.config.biocache.service + grailsApplication.config.biocache.search + '?' + query.join('&')
         def response = url.toURL().getText("UTF-8")
         return slurper.parseText(response)
